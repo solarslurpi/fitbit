7f6878bca605c2614b401b5f7a6e90c58d2a5f1b
Happyday <happyday.mjohnson@gmail.com>
2021-01-02 04:18:07 -0800

    played around with code

--- Fitbit_splashing.py
+++ Fitbit_splashing.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 my_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkJaS1YiLCJzdWIiOiIyM1ZCNTYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNjEwMDIyMjEwLCJpYXQiOjE2MDk0MTc0MTB9.m_vEY9pK3aVzC6cZ4353JtC23sGdzcG6FKWQ9DHpwaI"
 
+father_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkJaR00iLCJzdWIiOiIyNEtCM1MiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjEwMTQ5MTQ1LCJpYXQiOjE2MDk1NDQzNDV9.Gx6YJgJyNcyW12YimdAjw1ibs7djXrWPLy3efwBzwzA"
+
 
 def get_date_from_user():
 
@@ -18,7 +20,7 @@ def get_date_from_user():
         return datestr
 
 
-def get_data(date_str, activity='heart', detail_level='1min'):
+def get_data(date_str, activity='heart', detail_level='1sec'):
     """returns the fitbit time series data for the activity.
 
     :param date_str: date string with the format of YYYY-MM-DD.  The date should be a date that is
@@ -36,9 +38,9 @@ def get_data(date_str, activity='heart', detail_level='1min'):
     if date_str is None:
         raise Exception(
             "The function requires a date string in the format YYYY-MM-DD.")
-    heart_rates_url = "https://api.fitbit.com/1/user/-/activities/" \
-        + activity+"/date/" + date_str+"/1d/" + detail_level + ".json"
-    header = {'Authorization': 'Bearer {}'.format(my_access_token)}
+    heart_rates_url = "https://api.fitbit.com/1/user/-/activities/" + \
+        activity+"/date/" + date_str+"/1d/" + detail_level + ".json"
+    header = {'Authorization': 'Bearer {}'.format(father_access_token)}
     response = requests.get(heart_rates_url, headers=header).json()
     # If the request was successful, there should be an KeyError exception when asking for the errors list.
     try:
@@ -50,13 +52,20 @@ def get_data(date_str, activity='heart', detail_level='1min'):
 
 try:
     data_date_in_fitbit_fmt = get_date_from_user()
+    print(
+        f'The date you entered (converted to Fitbit format): {data_date_in_fitbit_fmt}')
     oneDayData = get_data(data_date_in_fitbit_fmt)
 except Exception as error:
     print(error)
 else:
     hr_df = pd.DataFrame(oneDayData['activities-heart-intraday']['dataset'])
-    hr_max = hr_df['value'].max()
-    hr_min = hr_df['value'].min()
-    hr_mean = hr_df['value'].mean()
-    print(hr_df.head())
-    print(f'Total HR Samples is {len(hr_df.index)}')
+    if hr_df.empty:
+        print(
+            f'No entries found for the date {data_date_in_fitbit_fmt}')
+    else:
+        hr_max = hr_df['value'].max()
+        hr_min = hr_df['value'].min()
+        hr_mean = hr_df['value'].mean()
+        print(hr_df.head())
+        print(f'min: {hr_min} max: {hr_max} average: {hr_mean}')
+        print(f'Total HR Samples is {len(hr_df.index)}')
